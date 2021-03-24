e939a22a01c7a68ba6d5e4bd6b1a3885cbc3b00d
unknown <paulo.ricardo.marolla@hotmail.com>
2021-03-24 20:46:54 -0300

    finalizacao buttonsubscribe

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,6 @@
 
 import Head from 'next/head'
+import { SubscribeButton } from '../components/SubscribeButton';
 import styles from './home.module.scss';
 
 export default function Home() {
@@ -12,7 +13,9 @@ export default function Home() {
         <span>👏 Hey, welcome</span>
         <h1>News about the <span>React</span> world.</h1>
         <p>Get access to all the publications <br/>
-        <span>for $9.90 month</span> </p>
+          <span>for $9.90 month</span> 
+        </p>
+        <SubscribeButton />
       </section>
 
       <img src="/images/avatar.svg" alt="Girl coding"/>
