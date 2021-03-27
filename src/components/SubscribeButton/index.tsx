837c268a9508d8e057a9b0d11eb5d49a03ccc139
unknown <paulo.ricardo.marolla@hotmail.com>
2021-03-27 12:41:11 -0300

    inicio do pagamento com stripe

--- src/components/SubscribeButton/index.tsx
+++ src/components/SubscribeButton/index.tsx
@@ -1,3 +1,4 @@
+import { useSession, signIn } from 'next-auth/client'
 import styles from './styles.module.scss'
 
 interface SusbcribeButtonProps {
@@ -5,8 +6,24 @@ interface SusbcribeButtonProps {
 }
 
 export const SubscribeButton = ({priceId}: SusbcribeButtonProps) => {
+  const [session] = useSession()
+
+  function handleSubscribe() {
+    if(!session) {
+      signIn('github')
+      return
+    }
+
+    
+
+  }
+
   return (
-    <button type="button" className={styles.subscribeButton}>
+    <button 
+    type="button" 
+    className={styles.subscribeButton}
+    onClick={handleSubscribe}
+    >
       Subscribe Now
     </button>
   )
