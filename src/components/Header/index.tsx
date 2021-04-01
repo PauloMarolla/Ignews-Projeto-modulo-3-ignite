0e9dbb3c6c15b215d7c0f93294ff70c891603a47
unknown <paulo.ricardo.marolla@hotmail.com>
2021-04-01 19:46:38 -0300

    mantendo o link ativo

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -1,14 +1,24 @@
 import { SignInButton } from '../SignInButton';
 import styles from './styles.module.scss';
+import { ActiveLink } from '../ActiveLink';
 
 export const Header = () => {
+  
   return (
     <header className={styles.headerContainer} >
       <div className={styles.headerContent}>
         <img src="/images/logo.svg" alt="Ig News"/>
         <nav>
-          <a className={styles.active} href="">Home</a>
-          <a href="">Posts</a>
+
+          <ActiveLink 
+          activeClassName={styles.active} href="/">
+            <a>Home</a>
+          </ActiveLink>
+
+          <ActiveLink 
+          activeClassName={styles.active} href="/posts" prefetch>
+            <a>Posts</a>
+          </ActiveLink>
         </nav>
         <SignInButton />
       </div>
